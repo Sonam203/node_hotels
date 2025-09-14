849b4a474e50da29c8857e174802ffb4a6f48d03
Sonam203 <sonam.it2022-23@recabn.ac.in>
2025-09-14 20:59:18 +0530

    shift mongodb to online server

--- server.js
+++ server.js
@@ -80,9 +80,11 @@ console.log(filter)  */
 const express = require('express')
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.json()); // req.body
+const PORT = process.env.PORT || 3000;
 
 
 
@@ -101,6 +103,8 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 // Use the routers
 app.use('/person' , personRoutes);
 app.use('/menu' , menuItemRoutes);
+
+
 
 app.listen(3000,()=>{
     console.log('listening on port 3000');
